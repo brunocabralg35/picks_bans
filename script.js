9df98cbb8c05016f8a8618a2e4ed5bbfacdb744a
Bruno Cabral <brunocabral035@gmail.com>
2023-03-31 17:19:19 -0300

    chaged the logics of condition, using obj property isBanned

--- script.js
+++ script.js
@@ -2,40 +2,55 @@ const mapPool = [
   {
     map: "ANCIENT",
     img: "./assets/ancient.jpeg",
+    isBanned: false,
   },
   {
     map: "ANUBIS",
     img: "./assets/anubis.jpeg",
+    isBanned: false,
   },
   {
     map: "INFERNO",
     img: "./assets/inferno.jpeg",
+    isBanned: false,
   },
   {
     map: "MIRAGE",
     img: "./assets/mirage.jpeg",
+    isBanned: false,
   },
   {
     map: "NUKE",
     img: "./assets/nuke.jpeg",
+    isBanned: false,
   },
   {
     map: "OVERPASS",
     img: "./assets/overpass.jpeg",
+    isBanned: false,
   },
   {
     map: "VERTIGO",
     img: "./assets/vertigo.jpeg",
+    isBanned: false,
   },
 ];
 
-let mapasRestastes = mapPool;
-let count = 7;
+let mapasRestastes = [];
 
 function vetaMapa(id) {
-  if (count == 1) {
+
+  if(mapasRestastes.length == 1){
     return;
   }
+
+  mapPool[id].isBanned = true;
+
+  mapasRestastes = mapPool.filter((item) => item.isBanned !== true);
+
+  document.getElementById(id).removeAttribute("onclick")
+
+  console.log(mapasRestastes);
 
   if (document.getElementById("turn-team").innerText === "TIME 1") {
     document.getElementById("turn-team").innerText = "TIME 2";
@@ -43,24 +58,22 @@ function vetaMapa(id) {
     document.getElementById("turn-team").innerText = "TIME 1";
   }
 
-  delete mapasRestastes[id];
-  count--;
-
   document.getElementById(id).classList.add("vetado");
   document.getElementById(id).classList.remove("map");
 
-  if (count == 1) {
-    let mapaDaPartida = mapasRestastes.filter((item) => item !== undefined);
-    console.log(mapaDaPartida);
+  if (mapasRestastes.length == 1) {
+    console.log(mapasRestastes[0].map);
     let newID = 0;
     document.getElementById(
       "turn"
-    ).innerText = `O mapa da partida é ${mapaDaPartida[0].map}`;
-    mapasRestastes.forEach((mapa, index) => {
-      if (mapa !== undefined) {
-        newID = index;
+    ).innerText = `O mapa da partida é ${mapasRestastes[0].map}`;
+
+    mapPool.forEach((map, i) => {
+      if (map.map === mapasRestastes[0].map) {
+        newID = i;
       }
     });
+
     document.getElementById(newID).classList.remove("map");
     document.getElementById(newID).classList.add("picked");
   }
